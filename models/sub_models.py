7e18332263c52317745820b6ee8cf98c99f92f26
fdy <1484231731@qq.com>
2022-10-13 20:52:13 +0800

    Initial commit

---
+++ models/sub_models.py
@@ -0,0 +1,340 @@
+"""
+Implementation of ``nn.Modules`` for temporal fusion transformer.
+"""
+import math
+from typing import Dict, List, Tuple, Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResampleNorm(nn.Module):
+    def __init__(self, input_size: int, output_size: int = None, trainable_add: bool = True):
+        super().__init__()
+
+        self.input_size = input_size
+        self.trainable_add = trainable_add
+        self.output_size = output_size or input_size
+
+        if self.input_size != self.output_size:
+            self.resample = TimeDistributedInterpolation(self.output_size, batch_first=True, trainable=False)
+
+        if self.trainable_add:
+            self.mask = nn.Parameter(torch.zeros(self.output_size, dtype=torch.float))
+            self.gate = nn.Sigmoid()
+        self.norm = nn.LayerNorm(self.output_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.input_size != self.output_size:
+            x = self.resample(x)
+
+        if self.trainable_add:
+            x = x * self.gate(self.mask) * 2.0
+
+        output = self.norm(x)
+        return output
+
+
+class TimeDistributedInterpolation(nn.Module):
+    """时间分布差值"""
+
+    def __init__(self, output_size: int, batch_first: bool = False, trainable: bool = False):
+        super().__init__()
+        self.output_size = output_size
+        self.batch_first = batch_first
+        self.trainable = trainable
+        if self.trainable:
+            self.mask = nn.Parameter(torch.zeros(self.output_size, dtype=torch.float32))
+            self.gate = nn.Sigmoid()
+
+    def interpolate(self, x):
+        upsampled = F.interpolate(x.unsqueeze(1), self.output_size, mode="linear", align_corners=True).squeeze(1)
+        if self.trainable:
+            upsampled = upsampled * self.gate(self.mask.unsqueeze(0)) * 2.0
+        return upsampled
+
+    def forward(self, x):
+
+        if len(x.size()) <= 2:
+            return self.interpolate(x)
+
+        # Squash samples and timesteps into a single axis
+        x_reshape = x.contiguous().view(-1, x.size(-1))  # (samples * timesteps, input_size)
+
+        y = self.interpolate(x_reshape)
+
+        # We have to reshape Y
+        if self.batch_first:
+            y = y.contiguous().view(x.size(0), -1, y.size(-1))  # (samples, timesteps, output_size)
+        else:
+            y = y.view(-1, x.size(1), y.size(-1))  # (timesteps, samples, output_size)
+
+        return y
+
+
+class AddNorm(nn.Module):
+    """这个类的主要作用是进行残差连接和层归一化，中间有一些别的东西"""
+    def __init__(self, input_size: int, skip_size: int = None, trainable_add: bool = True):
+        """
+        :param input_size:
+        :param skip_size:
+        :param trainable_add: 作用不知道
+        """
+        super().__init__()
+
+        self.input_size = input_size
+        self.skip_size = skip_size or input_size  # 如果skip_size是None，则self.skip_size = input_size
+        self.trainable_add = trainable_add
+
+        if self.input_size != self.skip_size:
+            self.resample = TimeDistributedInterpolation(self.input_size, batch_first=True, trainable=False)
+
+        if self.trainable_ad:
+            self.mask = nn.Parameter(torch.zeros(self.input_size, dtype=torch.float))
+            self.gate = nn.Sigmoid()
+
+        self.norm = nn.LayerNorm(self.input_size)
+
+    def forward(self, x: torch.Tensor, skip: torch.Tensor):
+        """skip这个参数是一定要传进来的，暂时还不知道是什么东西"""
+        if self.input_size != self.skip_size:
+            skip = self.resample(skip)
+
+        if self.trainable_add:
+            skip = skip * self.gate(self.mask) * 2.0
+
+        output = self.norm(x + skip)
+        return output
+
+
+class GatedLinearUnit(nn.Module):
+    """Gated Linear Unit 是对glu的封装，添加了dropout和Linear层"""
+
+    def __int__(self, input_size: int, hidden_size: int=None, dropout: float=None):
+        super().__init__()
+
+        if dropout is not None:
+            self.dropout = nn.Dropout(dropout)
+        else:
+            self.dropout = dropout
+        self.hidden_size = hidden_size or input_size
+        self.fc = nn.Linear(input_size. self.hidden_size * 2) # 这个地方不知道为什么要 *2
+
+        self.init_weights()  # 这就是执行初始化的步骤
+
+    def init_weights(self):
+        for n, p in self.named_parameters():
+            if "bias" in n:
+                torch.nn.init.zeros_(p)   # 将p使用0初始化
+            elif "fc" in n:  # 将线性层的参数使用 均匀分布采样初始化
+                torch.nn.init.xavier_uniform_(p)
+
+    def forward(self, x):
+        if self.dropout is not None:
+            x = self.dropout(x)
+        x = self.fc(x)
+        x = F.glu(x, dim=-1)
+        return x
+
+
+class GateAddNorm(nn.Module):
+    def __init__(
+            self,
+            input_size: int,
+            hidden_size: int=None,
+            skip_size: int=None,
+            trainable_add: bool=False,
+            dropout: float=None,
+    ):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size or input_size
+        self.skip_size = skip_size or self.hidden_size
+        self.dropout = dropout
+
+        self.glu = GatedLinearUnit(self.input_size, hidden_size=self.hidden_size, dropout=self.dropout)
+        self.add_norm = AddNorm(self.hidden_size, skip_size=self.skip_size, trainable_add=trainable_add)
+
+    def forward(self, x, skip):
+        output = self.glu(x)
+        output = self.add_norm(output, skip)
+        return output
+
+
+class GatedResidualNetwork(nn.Module):
+    """Gated Residual Network(GRN)"""
+    def __ini__(
+            self,
+            input_size: int,
+            hidden_size: int,
+            output_size: int,
+            dropout: float=0.1,
+            context_size: int=None,
+            residual: bool=False,
+    ):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.context_size = context_size
+        self.hidden_size = hidden_size
+        self.dropout = dropout
+        self.residual = residual
+
+        if self.input_size != self.output_size and not self.residual:
+            residual_size = self.input_size
+        else:
+            residual_size = self.output_size
+
+        if self.output_size != residual_size:
+            self.resample_norm = ResampleNorm(residual_size, self.output_size)
+
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
+        self.elu = nn.ELU()
+
+        if self.context_size is not None:
+            self.context = nn.Linear(self.context_size, self.hidden_size, bias=False)
+
+        self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.init_weights()
+
+        self.gate_norm = GateAddNorm(
+            input_size = self.hidden_size,
+            hidden_size=self.output_size,
+            skip_size=self.output_size,
+            dropout=self.dropout,
+            trainable_add=False,
+        )
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if "bias" in name:
+                torch.nn.init.zeros_(p)
+            elif "fc1" in name or "f2" in name:
+                torch.nn.init.kaiming_normal_(p, a=0, mode="fan_in", nonlinearity="leaky_relu")
+            elif "context" in name:
+                torch.nn.init.xavier_uniform_(p)
+
+    def forward(self, x, context=None, residual=None):
+        if residual is None:
+            residual = x
+        if self.input_size != self.output_size and not self.residual:
+            residual = self.resample_norm(residual)
+
+        x = self.fc1(x)
+        if context is not None:
+            context = self.context(context)
+            x = x + context
+        x = self.elu(x)
+        x = self.fc2(x)
+        x = self.gate_norm(x, residual)
+        return x
+
+
+class PositionalEncoder(torch.nn.Module):
+    """位置编码器"""
+    def __init__(self, d_model, max_seq_len=160):
+        super().__init__()
+        assert d_model % 2 == 0, "model dimension has to be multiple of 2 (encode sin(pos) and cos(pos))"
+        self.d_model = d_model
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        with torch.no_grad():
+            x = x * math.sqrt(self.d_model)
+            seq_len = x.size(0)
+            pe = self.pe[:, :seq_len].view(seq_len, 1, self.d_model)
+            x = x + pe
+            return x
+
+
+
+class ScaledDotProductAttention(nn.Module):
+    """点缩积注意力机制"""
+    def __init__(self, dropout: float = None, scale: bool = True):
+        super().__init__()
+        if dropout is not None:
+            self.dropout = nn.Dropout(p=dropout)
+        else:
+            self.dropout = dropout
+        self.softmax = nn.Softmax(dim=2)
+        self.scale = scale
+
+    def forward(self, q, k, v, mask=None):
+        attn = torch.bmm(q, k.permute(0, 2, 1))
+        # torch.bmm两个tensor的矩阵乘法，两个矩阵的一个维度要相同，然后第一个矩阵的第三个维度要和第二个矩阵的第二个维度相同
+        # tensor.permute交换tensor的维度顺序
+        if self.scale:
+            dimension = torch.as_tensor(k.size(-1), dtype=attn.dtype, device=attn.device).sqrt()
+            attn = attn / dimension
+
+        if mask is not None:
+            attn = attn.mask_fill(mask, -1e9)
+        attn = self.softmax(attn)
+
+        if self.dropout is not None:
+            attn = self.dropout(attn)
+        output = torch.bmm(attn, v)  # 这里的V和注意力矩阵相乘不需要更换维度位置吗
+        return output, attn
+
+
+class InterpretableMultiHeadAttention(nn.Module):
+    """可解释多头之注意力机制"""
+    def __init__(self, n_head: int, d_model: int, dropout: float=0.0):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_k = self.d_q = self.d_v = d_model // n_head
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.v_layer = nn.Linear(self.d_model, self.d_v)
+        self.q_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_q) for _ in range(self.n_head)])
+        self.k_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_k) for _ in range(self.n_head)])
+        self.attention = ScaledDotProductAttention()
+        self.w_h = nn.Linear(self.d_v, self.d_model, bias=False)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if "bias" not in name:
+                torch.nn.init.xavier_uniform_(p)
+            else:
+                torch.nn.init.zeros_(p)
+
+    def forward(self, q, k, v, mask=None) -> Tuple[torch.Tensor, torch.Tensor]:
+        heads = []
+        attns = []
+        vs = self.v_layer(v)
+        for i in range(self.n_head):
+            qs = self.q_layers[i](q)
+            ks = self.k_layers[i](k)
+            head, attn = self.attention(qs, ks, vs, mask)
+            head_dropout = self.dropout(head)
+            heads.append(head_dropout)
+            attns.append(attn)
+
+        head = torch.stack(heads, dim=2) if self.n_head > 1 else heads[0]
+        attn = torch.stack(attns, dim=2)
+
+        outputs = torch.mean(head, dim=2) if self.n_head > 1 else head
+        outputs = self.w_h(outputs)
+        outputs = self.dropout(outputs)
+
+        return outputs, attn
+
+
+class VariableSelectionNetwork(nn.Module):
+    def __init__(
+            self,
+            input_sizes: 
+    ):
+
+
---
+++ models/temporal_fusion_transforme.py
@@ -0,0 +1,7 @@
+from copy import copy
+from typing import Dict, List, Tuple, Union
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+from torch import nn
+from torchmetrics import Metric as LightningMetric
